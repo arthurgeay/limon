6af2d5791cd1549e9299aafb750c7f00d5d76c1e
arthurgeay <arthurgeay.contact@gmail.com>
2020-05-18 22:17:30 +0200

    Add apache pack and create movie controller

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MovieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/movie", name="movie_")
+ */
+class MovieController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search")
+     */
+    public function search(MovieRepository $movieRepository)
+    {
+        $movies = $movieRepository->findAll();
+        return $this->json($movies);
+    }
+}
