c40f0b7cc5d270537de941492337583a5a843d77
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-21 00:42:55 +0200

    Adding Catalog

--- limon/src/app/film-card/film-card.component.ts
+++ limon/src/app/film-card/film-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-film-card',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./film-card.component.scss']
 })
 export class FilmCardComponent implements OnInit {
+  @Input() card: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.card)
   }
 
 }
--- limon/src/app/film-result/film-result.component.ts
+++ limon/src/app/film-result/film-result.component.ts
@@ -10,6 +10,56 @@ export class FilmResultComponent implements OnInit {
   public isAlph = false;
   public isPrice = false;
   public isDate = false;
+  public catalog = [
+    {
+      poster:'../../assets/films/1917-poster.jpg',
+      title: '1917',
+      prod:'Universal Studios',
+      price: ''
+    },
+    {
+      poster:'../../assets/films/batman-poster.jpg',
+      title: 'The Dark Knights: Le chevalier noir',
+      prod:'Warner Bros',
+      price: '4.45 €'
+    },
+    {
+      poster:'../../assets/films/avengers-poster.jpg',
+      title: 'Avengers: Endgame',
+      prod:'Marvel Cinematic Universe',
+      price: '3.22€'
+    },
+    {
+      poster:'../../assets/films/godzilla-poster.jpg',
+      title: 'Godzilla',
+      prod:'Warner Bros',
+      price: ''
+    },
+    {
+      poster:'../../assets/films/1917-poster.jpg',
+      title: '1917',
+      prod:'Universal Studios',
+      price: ''
+    },
+    {
+      poster:'../../assets/films/batman-poster.jpg',
+      title: 'The Dark Knights: Le chevalier noir',
+      prod:'Warner Bros',
+      price: '4.45 €'
+    },
+    {
+      poster:'../../assets/films/avengers-poster.jpg',
+      title: 'Avengers: Endgame',
+      prod:'Marvel Cinematic Universe',
+      price: '3.22€'
+    },
+    {
+      poster:'../../assets/films/godzilla-poster.jpg',
+      title: 'Godzilla',
+      prod:'Warner Bros',
+      price: ''
+    }
+  ]
 
   constructor() { }
 
