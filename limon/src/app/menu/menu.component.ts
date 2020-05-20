d5513f9780214135198f19d5afb53ffc8916c701
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-20 15:01:46 +0200

    Responsive Menu

--- limon/src/app/menu/menu.component.ts
+++ limon/src/app/menu/menu.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  public isMobile = false;
+  public isMenu = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.isMobile = this.getIsMobile();
+    window.onresize = () => this.isMobile = window.innerWidth <= 665;
+  }
+  getIsMobile(): boolean {
+    return(document.documentElement.clientWidth < 665 ? true : false);
+  }
+  onAppear() {
+    this.isMenu = !this.isMenu;
+  }
+  onMenuOpen(){
+    return(this.isMenu ? 'translateY(0rem)' : 'translateY(-12rem)')
   }
 
 }
