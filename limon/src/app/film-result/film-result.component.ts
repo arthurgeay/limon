1d14f30cb13359567aa67dcfa96cfd8bbaa62106
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-28 22:47:53 +0200

    Scrolling buttons on catalogue first view

--- limon/src/app/film-result/film-result.component.ts
+++ limon/src/app/film-result/film-result.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { MovieService } from '../movie.service';
 import { MobileService } from '../mobile.service';
 
@@ -10,12 +10,20 @@ import { MobileService } from '../mobile.service';
 export class FilmResultComponent implements OnInit {
 
   public isEmpty: boolean = false;
+  public isSearch: boolean = false;
   public isAlph = false;
   public isPrice = false;
   public isDate = false;
   public catalog: any[];
   public isMobile: boolean = false;
   public isRGPD:boolean;
+  @ViewChild('scroll1', { read: ElementRef }) public scroll1: ElementRef<any>;
+  @ViewChild('scroll2', { read: ElementRef }) public scroll2: ElementRef<any>;
+  scrollToLeft1 = false;
+  scrollToRight1 = true;
+  scrollToLeft2 = false;
+  scrollToRight2 = true;
+
 
   constructor(private movieService: MovieService,
     private mobileService:MobileService) { }
@@ -36,6 +44,53 @@ export class FilmResultComponent implements OnInit {
 
   onDate() {
     return(this.isDate = !this.isDate)
+  }
+  scroll1Start() {
+    this.scroll1.nativeElement.scrollLeft -= this.scroll1.nativeElement.clientWidth;
+  }
+
+  scroll2Start() {
+    this.scroll2.nativeElement.scrollLeft -= this.scroll2.nativeElement.clientWidth;
+  }
+
+  scroll1End() {
+    this.scroll1.nativeElement.scrollLeft += this.scroll1.nativeElement.clientWidth;
+  }
+
+  scroll2End() {
+    this.scroll2.nativeElement.scrollLeft += this.scroll2.nativeElement.clientWidth;
+  }
+
+  onScroll1(event:Event) {
+    if (event.srcElement.scrollLeft >= 100) {
+      this.scrollToLeft1 = true;
+    }
+    else {
+      this.scrollToLeft1 = false;
+    }
+
+    if (event.srcElement.scrollLeft >= this.scroll1.nativeElement.clientWidth / 10) {
+      this.scrollToRight1 = false;
+    }
+    else {
+      this.scrollToRight1 = true;
+    }
+  }
+
+  onScroll2(event:Event) {
+    if (event.srcElement.scrollLeft >= 100) {
+      this.scrollToLeft2 = true;
+    }
+    else {
+      this.scrollToLeft2 = false;
+    }
+
+    if (event.srcElement.scrollLeft >= this.scroll2.nativeElement.clientWidth / 10) {
+      this.scrollToRight2 = false;
+    }
+    else {
+      this.scrollToRight2 = true;
+    }
   }
 
   @HostListener('window:resize', ['$event'])
