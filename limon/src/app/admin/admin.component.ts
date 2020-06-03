29d653169e48f4a37a4a947300e68ff864b9a682
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-06-03 13:49:01 +0200

    t

--- limon/src/app/admin/admin.component.ts
+++ limon/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  public stats: any;
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get(`https://api-limon.app-tricycle.com/api/admin/stats`)
+    .subscribe(
+      (data:any)=>{
+        this.stats = data;       
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
   }
 
 }
