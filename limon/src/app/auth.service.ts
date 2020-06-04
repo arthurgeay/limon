ec45faadc214a1544a74b6b746382942b1ac0d54
arthurgeay <arthurgeay.contact@gmail.com>
2020-06-05 01:06:29 +0200

    Add roles in localstorage

--- limon/src/app/auth.service.ts
+++ limon/src/app/auth.service.ts
@@ -21,9 +21,17 @@ export class AuthService {
    * Save in local storage
    * @param token 
    */
-  private saveInLocalStorage(token, expires) {
+  private saveInLocalStorage(token, expires, roles) {
     localStorage.setItem('token', token);
-    localStorage.setItem('expires', expires.date);
+    localStorage.setItem('expires', expires);
+    localStorage.setItem('roles', roles);
+  }
+
+  /**
+   * Delete all info in localStorage
+   */
+  private deleteInLocalStorage() {
+    localStorage.clear();
   }
 
   /**
@@ -44,7 +52,7 @@ export class AuthService {
       password: user.password
     }).subscribe(
       (res: any) => {
-        this.saveInLocalStorage(res.token, res.expires);
+        this.saveInLocalStorage(res.token, res.expires.date, res.data.roles);
 
         this.userService.user = { ...res.data };
         this.userService.emitUserSubject();
@@ -75,7 +83,7 @@ export class AuthService {
 
     this.httpClient.post('https://api-limon.app-tricycle.com/api/register', formData).subscribe(
       (res: any) => {
-        this.saveInLocalStorage(res.token, res.expires);
+        this.saveInLocalStorage(res.token, res.expires, res.user.roles);
         
         this.userService.user = { ...res.user };
         this.userService.emitUserSubject();
@@ -97,8 +105,7 @@ export class AuthService {
    * Log out a user
    */
   logout() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expires');
+    this.deleteInLocalStorage();
 
     this.userService.user = null;
     this.userService.emitUserSubject();
