c7d22415d7409c708a0454626f6e3c2f03484dd2
Arthur Geay <arthurgeay.contact@gmail.com>
2020-06-06 03:45:46 +0200

    Fix bug to download invoice

--- limon/src/app/playlist-item/playlist-item.component.ts
+++ limon/src/app/playlist-item/playlist-item.component.ts
@@ -61,16 +61,7 @@ export class PlaylistItemComponent implements OnInit {
 
   onBill() {
     let mediaType = 'application/pdf';
-    const httpOptions = {
-      headers: new HttpHeaders({
-        "Content-Type": "text/plain",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-        "Access-Control-Allow-Headers": "Origin,Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-      })
-    };
-    this.http.get(`https://api-limon.app-tricycle.com/api/purchase/invoice/${this.idBuy}`, httpOptions)
+    this.http.get(`https://api-limon.app-tricycle.com/api/purchase/invoice/${this.idBuy}`, { responseType: 'blob' })
     .subscribe(
       (data:any)=>{
         let blob = new Blob([data], { type:mediaType})
