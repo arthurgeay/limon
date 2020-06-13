4a24cb33a570fb170eff7a4b7808e76fba8897c3
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-06-13 18:10:44 +0200

    delete console.log

--- limon/src/app/playlist/playlist.component.ts
+++ limon/src/app/playlist/playlist.component.ts
@@ -64,7 +64,6 @@ export class PlaylistComponent implements OnInit {
           this.isEmpty = false;
           this.catalog = data;  
         }
-        console.log(data);
       },
       (error)=>{
         console.log(error);
@@ -87,7 +86,6 @@ export class PlaylistComponent implements OnInit {
           this.isEmpty = false;
           this.catalog = data;  
         }
-        console.log(data);
       },
       (error)=>{
         console.log(error);
@@ -110,7 +108,6 @@ export class PlaylistComponent implements OnInit {
           this.isEmpty = false;
           this.catalog = data;  
         }
-        console.log(data);
       },
       (error)=>{
         console.log(error);
